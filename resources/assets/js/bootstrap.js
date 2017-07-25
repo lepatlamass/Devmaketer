773e6e8184f94be23d430e6cf974b843df1a26ce
lepatlamass <kwonmazoukonwo@gmail.com>
2017-07-25 17:20:05 +0100

    setting up our laravel app to start coding wiht js and css

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,4 +1,3 @@
-
 window._ = require('lodash');
 
 /**
@@ -9,8 +8,6 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
 } catch (e) {}
 
 /**
